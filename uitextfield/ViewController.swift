483a390982bae90e734a99a832ceb1f94bc5147f
bondgeek <bartm@bondgeek.com>
2015-01-30 17:41:40 -0500

    working example

--- uitextfield/ViewController.swift
+++ uitextfield/ViewController.swift
@@ -7,8 +7,17 @@
 //
 import UIKit
 
-/*
-class myTextField: UITextField {
+
+@IBDesignable class myTextField: UITextField {
+    var border = CALayer()
+    var bottomBorderWidth: CGFloat = 2.0
+    
+    @IBInspectable var borderColor: UIColor = UIColor.lightGrayColor() {
+        didSet {
+            border.borderColor = borderColor.CGColor
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -17,46 +26,64 @@ class myTextField: UITextField {
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
-        var border = CALayer()
-        var borderWidth = CGFloat(2.0)
-        border.borderColor = UIColor.greenColor().CGColor
+        
+        self.borderStyle = UITextBorderStyle.None
+        
+        border.borderWidth = bottomBorderWidth
         border.frame = CGRect(
             x: 0,
-            y: self.frame.size.height - borderWidth,
+            y: self.frame.size.height - self.bottomBorderWidth,
             width:  self.frame.size.width,
             height: self.frame.size.height
         )
-        border.borderWidth = borderWidth
+        
         self.layer.addSublayer(border)
         self.layer.masksToBounds = true
         
         println("coder")
-        println("bs: \(self.borderStyle.rawValue)")
+        println("border width \(self.bottomBorderWidth)")
         println(self.frame.size.width)
     }
 }
-*/
+
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textFieldOne: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         println("view did load")
+        println("view size")
+        println("x: \(view.frame.origin.x) y: \(view.frame.origin.y)")
+        println("w: \(view.frame.size.width) h: \(view.frame.size.height)")
         
-        /*
+        textFieldOne.frame.size.width = 270
+        
+        println("Frame")
+        println("x: \(textFieldOne.frame.origin.x) y: \(textFieldOne.frame.origin.y)")
+        println("w: \(textFieldOne.frame.size.width) h: \(textFieldOne.frame.size.height)")
+        
+        println("Bounds")
+        println("x: \(textFieldOne.bounds.origin.x) y: \(textFieldOne.bounds.origin.y)")
+        println("w: \(textFieldOne.bounds.size.width) h: \(textFieldOne.bounds.size.height)")
+        
+        
         var border = CALayer()
-        var borderWidth = CGFloat(2.0)
+        var borderWidth = CGFloat(1.0)
         border.borderColor = UIColor.greenColor().CGColor
         border.frame = CGRect(
             x: 0,
-            y: textField1.frame.size.height - borderWidth,
-            width:  textField1.frame.size.width,
-            height: textField1.frame.size.height
+            y: textFieldOne.frame.size.height - borderWidth,
+            width:  textFieldOne.frame.size.width,
+            height: textFieldOne.frame.size.height
         )
+
         border.borderWidth = borderWidth
-        textField1.layer.addSublayer(border)
-        textField1.layer.masksToBounds = true
-        */
+        textFieldOne.borderStyle = UITextBorderStyle.None
+        textFieldOne.layer.addSublayer(border)
+        textFieldOne.layer.masksToBounds = true
+        
 
         
     }
